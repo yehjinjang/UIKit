8b38bfccf6fe21b35ed7096b65a7db9ff5339c27
yehjinjang <j95033176@gmail.com>
2024-05-20 11:31:38 +0900

    Stackview 사용

--- StackView/StackView/ViewController.swift
+++ StackView/StackView/ViewController.swift
@@ -11,7 +11,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        
+        let  topLabel = UILabel()
+        topLabel.text = "top"
+        topLabel.textAlignment = .center
+        topLabel.backgroundColor = .lightGray
+        
+        let leftLabel = UILabel()
+        leftLabel.text = "left"
+        leftLabel.textAlignment = .center
+        leftLabel.backgroundColor = .lightGray
+                
+        let rightLabel = UILabel()
+        rightLabel.text = "right"
+        rightLabel.textAlignment = .center
+        rightLabel.backgroundColor = .lightGray
+        
+        let hStackView = UIStackView(arrangedSubviews: [leftLabel, rightLabel])
+        hStackView.axis = .horizontal
+        hStackView.spacing = 10
+        
+        stackView.addArrangedSubview(topLabel)
+        stackView.addArrangedSubview(hStackView)
+        
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.widthAnchor.constraint(equalToConstant: 200),
+            stackView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
+
     }
 
 
