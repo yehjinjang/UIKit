dd8b9dd2165f0f579b4714f7d8371318398e7eb2
yehjinjang <j95033176@gmail.com>
2024-05-20 13:34:00 +0900

    UIStepper

--- StackView/StackView/ViewController.swift
+++ StackView/StackView/ViewController.swift
@@ -8,53 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let stepper = UIStepper()
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let stackView = UIStackView()
+        
+        label.text = "값: 0"
+        
+        stepper.minimumValue = 0
+        stepper.maximumValue = 10
+        stepper.stepValue = 1
+        stepper.value = 0
+        stepper.addAction(UIAction {[weak self] _ in
+            self?.label.text = "값: \(self?.stepper.value ?? 0)"
+        }, for: .valueChanged)
+        
+        let stackView = UIStackView(arrangedSubviews: [label, stepper])
         stackView.axis = .vertical
         stackView.spacing = 10
         stackView.alignment = .center
-        stackView.distribution = .fillEqually
-        
-        let  topLabel = UILabel()
-        topLabel.text = "top"
-        topLabel.textAlignment = .center
-        topLabel.backgroundColor = .lightGray
-        
-        let leftLabel = UILabel()
-        leftLabel.text = "left"
-        leftLabel.textAlignment = .center
-        leftLabel.backgroundColor = .lightGray
-                
-        let rightLabel = UILabel()
-        rightLabel.text = "right"
-        rightLabel.textAlignment = .center
-        rightLabel.backgroundColor = .lightGray
-        
-        let hStackView = UIStackView(arrangedSubviews: [leftLabel, rightLabel])
-        hStackView.axis = .horizontal
-        hStackView.spacing = 10
-        
-        stackView.addArrangedSubview(topLabel)
-        stackView.addArrangedSubview(hStackView)
         
         view.addSubview(stackView)
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        
         NSLayoutConstraint.activate([
-            
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.widthAnchor.constraint(equalToConstant: 200),
-            stackView.heightAnchor.constraint(equalToConstant: 100)
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
-        
-
     }
-
-
 }
-
