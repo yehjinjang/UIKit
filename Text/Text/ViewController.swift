e89242a4aeebac66944351a7bad8b12f109c8cc1
yehjinjang <j95033176@gmail.com>
2024-05-20 16:12:23 +0900

    various text style

--- Text/Text/ViewController.swift
+++ Text/Text/ViewController.swift
@@ -12,36 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let paddingView = UIView()
-        paddingView.translatesAutoresizingMaskIntoConstraints = false
-        paddingView.backgroundColor = .blue
+        let attributedString = NSMutableAttributedString(string: "Hello, World")
+        attributedString.addAttribute(.font, value: UIFont.italicSystemFont(ofSize: 20), range: NSRange(location: 0, length: 5))
+        attributedString.addAttribute(.foregroundColor, value: UIColor.blue, range: NSRange(location: 7, length: 5))
+        attributedString.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: 12))
         
         let label = UILabel()
-        label.text = "Stylized Text"
-        label.font = UIFont.boldSystemFont(ofSize: 17)
-        label.textColor = .yellow
-        label.backgroundColor = .red
-        label.textAlignment = .center
-        
-        label.layer.cornerRadius = 10
-        label.clipsToBounds = true
-        
+        label.attributedText = attributedString
         label.translatesAutoresizingMaskIntoConstraints = false
         
-        
-        paddingView.addSubview(label)
-        view.addSubview(paddingView)
+        view.addSubview(label)
         
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: paddingView.topAnchor, constant: 20),
-            label.bottomAnchor.constraint(equalTo: paddingView.bottomAnchor, constant: -20),
-            label.leadingAnchor.constraint(equalTo: paddingView.leadingAnchor, constant: 20),
-            label.trailingAnchor.constraint(equalTo: paddingView.trailingAnchor, constant: -20),
-            
-            paddingView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            paddingView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
-            paddingView.widthAnchor.constraint(equalToConstant: 200),
-            paddingView.heightAnchor.constraint(equalToConstant: 100)
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200)
         ])
     }
 
