b7a2104fa0b27525d0e70a66b6457ed2c887e55b
yehjinjang <j95033176@gmail.com>
2024-05-20 17:19:47 +0900

    Image

---
+++ Image/Image/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image
+//
+//  Created by 장예진 on 5/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
