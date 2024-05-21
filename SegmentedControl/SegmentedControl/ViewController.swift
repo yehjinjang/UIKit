a2eaa7c9ef30d41b8a802cbfdd3e4f714042d9f3
yehjinjang <j95033176@gmail.com>
2024-05-21 09:43:32 +0900

    UIButton with configuration

--- SegmentedControl/SegmentedControl/ViewController.swift
+++ SegmentedControl/SegmentedControl/ViewController.swift
@@ -9,34 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
     var count = 0
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let button = UIButton(type: .system)
-
-        button.setTitleColor(.white, for : .normal)
-        button.backgroundColor  = .systemBlue
-        button.layer.cornerRadius = 10
-        button.layer.borderWidth   = 2
-        button.layer.borderColor = UIColor.white.cgColor
-        //padding function
-        button.contentEdgeInsets  = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
-        button.setTitle("Click me", for: .normal)
-        //self 를 쓰기 위해 약한 참조를 사용하는 클로저 사용함 >
-        // 여기서 안써도 되는 이유?  구문안에서 바깥쪽에있는 것을 표현하기 위해서 약한 참조를 쓰는것임 (아니라면 굳이?)
-        button.addAction(UIAction{ [weak self] _ in
-            self?.count += 1
-            print("Button already tapped! \(self?.count ?? 0 )")}
-                         , for: .touchUpInside)
         
-        button.translatesAutoresizingMaskIntoConstraints   = false
+        let button = UIButton(type: .custom)
+        // layer를 직접 access 하는 방식( 밑의 것은 config 를 씌운것) 
+//        button.setTitleColor(.white, for: .normal)
+//        button.backgroundColor = .systemBlue
+//        button.layer.cornerRadius = 10
+//        button.layer.borderWidth = 2
+//        button.layer.borderColor = UIColor.white.cgColor
+        var config = UIButton.Configuration.filled()
+        
+        config.title = "Click me"
+        config.baseBackgroundColor = .systemBlue
+        config.baseForegroundColor = .white
+        config.cornerStyle = .capsule
+
+        
+        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        button.configuration = config
+        
+//        button.setTitle("Click me", for: .normal)
+        button.addAction(UIAction { [weak self] _ in
+            self?.count += 1
+            print("Button was tapped! \(self?.count ?? 0)")
+        }, for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(button)
         
         NSLayoutConstraint.activate([
             button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
 
