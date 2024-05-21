bc401c25e5e29fd93823826b7a5b19ba3fff762d
yehjinjang <j95033176@gmail.com>
2024-05-21 10:15:43 +0900

    color

--- SegmentedControl/SegmentedControl/ViewController.swift
+++ SegmentedControl/SegmentedControl/ViewController.swift
@@ -8,48 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var count = 0
+    let segmentedControl = UISegmentedControl(items: ["Red", "Green", "Blue"])
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let button = UIButton(type: .custom)
-//        button.setTitleColor(.white, for: .normal)
-//        button.backgroundColor = .systemBlue
-//        button.layer.cornerRadius = 10
-//        button.layer.borderWidth = 2
-//        button.layer.borderColor = UIColor.white.cgColor
-        var config = UIButton.Configuration.filled()
-        
-        config.title = "Click me"
-        config.baseBackgroundColor = .systemBlue
-        config.baseForegroundColor = .white
-//        config.cornerStyle = .small
-
-        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
-        button.configuration = config
-        
-//        button.setTitle("Click me", for: .normal)
-        button.addAction(UIAction { [weak self] _ in
-            self?.count += 1
-            print("Button was tapped! \(self?.count ?? 0)")
-        }, for: .touchUpInside)
-        
-        button.translatesAutoresizingMaskIntoConstraints = false
-                
-        view.addSubview(button)
-        
-        let button2 = UIButton(type: .custom)
-        config.baseBackgroundColor = .systemRed
-        config.cornerStyle = .medium
-        button2.configuration = config
-        button2.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(button2)
-        
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.addAction(UIAction { [weak self] _ in
+            switch self?.segmentedControl.selectedSegmentIndex {
+            case 0:
+                self?.view.backgroundColor = .red
+            case 1:
+                self?.view.backgroundColor = .green
+            case 2:
+                self?.view.backgroundColor = .blue
+            default:
+                break
+            }
+        }, for: .valueChanged)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
         NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            button2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button2.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 10)
+            segmentedControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
         ])
     }
 
