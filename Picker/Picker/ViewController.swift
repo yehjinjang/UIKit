8aeeb7191a6c905a0fc25398528eabecc8d266ab
yehjinjang <j95033176@gmail.com>
2024-05-21 14:02:13 +0900

    DatePicker

--- Picker/Picker/ViewController.swift
+++ Picker/Picker/ViewController.swift
@@ -8,44 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    private lazy var pickerView: UIPickerView = {
-        let pickerView = UIPickerView()
-        pickerView.delegate = self
-        pickerView.dataSource = self
-        pickerView.translatesAutoresizingMaskIntoConstraints = false
-        return pickerView
-    }()
-    let options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]
+class ViewController: UIViewController {
     
+    let datePicker = {
+        let picker = UIDatePicker()
+        picker.translatesAutoresizingMaskIntoConstraints  = false
+        return picker
+    } ()
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(pickerView)
+        
+        let today = Date()
+        var dateComponents  = DateComponents()
+        dateComponents.year = 1
+        // 날 짜 컴포넌트의 일년 개념을 달력에 넣어줌
+        let oneYearFromNow = Calendar.current.date(byAdding: dateComponents, to : today)
+        
+        datePicker.minimumDate = today
+        datePicker.maximumDate = oneYearFromNow
+        
+        datePicker.addAction(UIAction{ [weak self] _ in
+            print("Sender: \(self?.datePicker.date.formatted() ?? "N/A")")
+        }, for : .valueChanged)
+        
+        
+        view.addSubview(datePicker)
         
         NSLayoutConstraint.activate([
-            pickerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            pickerView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
-
-    }
-
-    // MARK: - UIPickerViewDataSource
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        1
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        options.count
-    }
-    
-    
-    // MARK: - UIPickerViewDelegate
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return options[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print("selected row: \(row)")
-    }
 }
