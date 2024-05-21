7a554c2e5d90b831ef184f855eebdc6796f6852a
yehjinjang <j95033176@gmail.com>
2024-05-21 15:41:21 +0900

    사파리뷰컨트롤러추가

--- Link/Link/ViewController.swift
+++ Link/Link/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 import WebKit
 import SafariServices
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController {g
+    private lazy var webView: WKWebView = {
+        let webView = WKWebView(frame: .zero)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.isHidden = true
+        return webView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         let linkButton = UIButton(type: .system)
         var conf = UIButton.Configuration.filled()
@@ -23,18 +28,33 @@ class ViewController: UIViewController {
         
         linkButton.translatesAutoresizingMaskIntoConstraints = false
         
-        linkButton.addAction(UIAction { _ in
+        linkButton.addAction(UIAction { [weak self] _ in
             if let url = URL(string:"https://www.apple.com") {
-                UIApplication.shared.open(url)
+//                UIApplication.shared.open(url)
+                let safariViewController = SFSafariViewController(url: url)
+                // 다양한 형태의 모달 뷰로 띄울 수 있다.
+                safariViewController.modalPresentationStyle = .pageSheet
+                self?.present(safariViewController, animated: true)
+//                self?.openInWebView(url: url)
             }
         }, for: .touchUpInside)
         
         view.addSubview(linkButton)
-        
+        view.addSubview(webView)
+
         NSLayoutConstraint.activate([
             linkButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             linkButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
-        
+    }
+
+    func openInWebView(url: URL) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+        webView.isHidden = false
     }
 }
