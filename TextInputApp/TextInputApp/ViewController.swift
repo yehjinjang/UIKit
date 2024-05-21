68299afe850d480af05999402771ef4b3b1e3e23
yehjinjang <j95033176@gmail.com>
2024-05-21 11:06:13 +0900

    initialize project

---
+++ TextInputApp/TextInputApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextInputApp
+//
+//  Created by 장예진 on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
