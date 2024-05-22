d0a306d96a4ee7d564252d02a3e2d17bebf117f1
yehjinjang <j95033176@gmail.com>
2024-05-22 09:48:08 +0900

    PinchGesture

--- Gesture/Gesture/ViewController.swift
+++ Gesture/Gesture/ViewController.swift
@@ -12,22 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let rectangle = UIView()
-        rectangle.backgroundColor = .yellow
-        rectangle.frame = CGRect(x: 100, y: 100, width: 175, height: 125)
-        rectangle.isUserInteractionEnabled = true
+        let imageView = UIImageView(image: UIImage(systemName: "star.fill"))
+        imageView.frame = CGRect(x: 120, y: 300, width: 200, height: 200)
+        imageView.isUserInteractionEnabled = true
         
-        view.addSubview(rectangle)
+        view.addSubview(imageView)
         
-        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleGesture))
-        gesture.minimumPressDuration = 2.0
-        rectangle.addGestureRecognizer(gesture)
+        let gesture = UIPinchGestureRecognizer(target: self, action: #selector(handleGesture))
+        imageView.addGestureRecognizer(gesture)
     }
 
-    @objc func handleGesture(_ sender: UILongPressGestureRecognizer) {
-        print("\(sender.state)")
-        if let view = sender.view, sender.state == .began {
-            view.backgroundColor = (view.backgroundColor == .yellow) ? .red : .yellow
+    @objc func handleGesture(_ sender: UIPinchGestureRecognizer) {
+        print(sender.scale)
+        if let view = sender.view {
+            view.transform = view.transform.scaledBy(x: sender.scale, y: sender.scale)
         }
     }
 
