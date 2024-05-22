999547b90c24c2ce1f046379d777a832dd4f614e
yehjinjang <j95033176@gmail.com>
2024-05-22 14:23:35 +0900

    simpleList with TableView

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+     let items = ["cat","dog","bird","fish","reptile"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tableView = UITableView(frame: view.bounds)
+        tableView.dataSource = self
+        view.addSubview(tableView)
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default,
+                                                                                            reuseIdentifier: "cell")
+        var config = cell.defaultContentConfiguration()
+        return cell
+    }
 
 }
 
