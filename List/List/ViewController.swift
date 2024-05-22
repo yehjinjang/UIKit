60e5bfdeba75138709ecc4ccc631a5cedbc26764
yehjinjang <j95033176@gmail.com>
2024-05-22 14:44:16 +0900

    table with section +style

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -7,26 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+struct Animal {
+    let name: String
+}
 
-     let items = ["cat","dog","bird","fish","reptile"]
+struct AnimalCategory {
+    let category: String
+    let animals: [Animal]
+}
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let categories = [
+        AnimalCategory(category: "포유류", animals: [Animal(name: "고양이"), Animal(name: "강아지")]),
+        AnimalCategory(category: "파충류", animals: [Animal(name: "거북이"), Animal(name: "도마뱀")])
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tableView = UITableView(frame: view.bounds)
+        // 테이블 스타일 변경 가능 (생성자 함수에서)
+        let tableView = UITableView(frame: view.bounds, style: .insetGrouped)
         tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         view.addSubview(tableView)
     }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return categories.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return categories[section].animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return categories[section].category
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default,
-                                                                                            reuseIdentifier: "cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
         var config = cell.defaultContentConfiguration()
+        config.text = categories[indexPath.section].animals[indexPath.row].name
+        cell.contentConfiguration = config
         return cell
     }
+
 
 }
 
