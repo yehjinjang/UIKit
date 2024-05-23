2f0f43a6059a07ec5940f1d55fd0bef33c7ebfeb
yehjinjang <j95033176@gmail.com>
2024-05-23 10:11:07 +0900

    CustomCellRefactory

--- List/List/ViewController.swift
+++ List/List/ViewController.swift
@@ -7,6 +7,46 @@
 
 import UIKit
 
+class CustomCell: UITableViewCell {
+    let animalImageView = UIImageView()
+    let nameLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        // 안에 들어가는 이미지 크기를 프레임 비율에 맞춰 넣도록 설정
+        animalImageView.contentMode = .scaleAspectFit
+        animalImageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(animalImageView)
+        contentView.addSubview(nameLabel)
+        
+        NSLayoutConstraint.activate([
+            animalImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            animalImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            animalImageView.widthAnchor.constraint(equalToConstant: 50),
+            animalImageView.heightAnchor.constraint(equalToConstant: 50),
+
+            nameLabel.leadingAnchor.constraint(equalTo: animalImageView.trailingAnchor, constant: 16),
+            nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
+    func configure(item: Animal) {
+        animalImageView.image = item.image
+        nameLabel.text = item.name
+    }
+}
+
 struct Animal {
     let name: String
     let image: UIImage
@@ -24,7 +64,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         let tableView = UITableView(frame: view.bounds)
         tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(CustomCell.self, forCellReuseIdentifier: "cell")
         view.addSubview(tableView)
     }
     
@@ -34,11 +74,13 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        var config = cell.defaultContentConfiguration()
-        config.text = animals[indexPath.row].name
-        config.image = animals[indexPath.row].image
-        cell.contentConfiguration = config
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomCell
+//        var config = cell.defaultContentConfiguration()
+//        config.text = animals[indexPath.row].name
+//        config.image = animals[indexPath.row].image
+//        cell.contentConfiguration = config
+        let animal: Animal = animals[indexPath.row]
+        cell.configure(item: animal)
         return cell
     }
     
