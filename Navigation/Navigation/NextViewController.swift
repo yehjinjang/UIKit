d11b31f45ddeb184817438f155d997235608d1a8
yehjinjang <j95033176@gmail.com>
2024-05-23 17:41:03 +0900

    with protocol

--- Navigation/Navigation/NextViewController.swift
+++ Navigation/Navigation/NextViewController.swift
@@ -32,6 +32,24 @@ class NextViewController: UIViewController {
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
+        
+        let button = UIButton(type: .custom)
+        button.configuration = UIButton.Configuration.filled()
+        button.setTitle("send", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addAction(UIAction{ [ weak self] _ in
+            if let delegate = self?.delegate {
+                delegate.save(animal: self?.animal ?? Animal(name: "고라니"))
+                self?.navigationController?.popViewController(animated: true)
+            
+            }
+        }, for: .touchUpInside)
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 10)
+        ])
     }
     
 
