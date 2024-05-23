8e17af0de494eb188d763baca91d5caee51f2f5a
yehjinjang <j95033176@gmail.com>
2024-05-23 10:28:55 +0900

    Form UI construct

--- Form/Form/ViewController.swift
+++ Form/Form/ViewController.swift
@@ -8,12 +8,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let formOneLabel = UILabel()
+    let formOneTextField = UITextField()
+    let formTwoLabel = UILabel()
+    let formTwoTextField = UITextField()
+    let resultLabelOne = UILabel()
+    let resultLabelTwo = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        setupFormOne()
+        setupFormTwo()
+        setupResults()
+        
+        
+    }
+    
+    func setupFormOne() {
+        formOneLabel.text = "이것은 첫 번째 폼입니다"
+        formOneLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(formOneLabel)
+        
+        formOneTextField.borderStyle = .roundedRect
+        formOneTextField.placeholder = "여기에 입력하세요"
+        formOneTextField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(formOneTextField)
+        
+        NSLayoutConstraint.activate([
+            formOneLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            formOneLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            formOneTextField.topAnchor.constraint(equalTo: formOneLabel.bottomAnchor, constant: 10),
+            formOneTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            formOneTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+
+    }
+    
+    func setupFormTwo() {
+        formTwoLabel.text = "이것은 두 번째 폼입니다"
+        formTwoLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(formTwoLabel)
+        
+        formTwoTextField.borderStyle = .roundedRect
+        formTwoTextField.placeholder = "여기에 입력하세요"
+        formTwoTextField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(formTwoTextField)
+        
+        NSLayoutConstraint.activate([
+            formTwoLabel.topAnchor.constraint(equalTo: formOneTextField.bottomAnchor, constant: 20),
+            formTwoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            formTwoTextField.topAnchor.constraint(equalTo: formTwoLabel.bottomAnchor, constant: 10),
+            formTwoTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            formTwoTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    func setupResults() {
+        resultLabelOne.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(resultLabelOne)
+        
+        resultLabelTwo.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(resultLabelTwo)
+        
+        NSLayoutConstraint.activate([
+            resultLabelOne.topAnchor.constraint(equalTo: formTwoTextField.bottomAnchor, constant: 20),
+            resultLabelOne.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            resultLabelTwo.topAnchor.constraint(equalTo: resultLabelOne.bottomAnchor, constant: 10),
+            resultLabelTwo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ])
     }
 
-
 }
-
