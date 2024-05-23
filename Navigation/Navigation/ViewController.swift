d0e30a16c5c44ce37728246f1f6f1fd54ce31ad9
yehjinjang <j95033176@gmail.com>
2024-05-23 17:09:53 +0900

    navigation bar color change with code

--- Navigation/Navigation/ViewController.swift
+++ Navigation/Navigation/ViewController.swift
@@ -8,19 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // 둘 다 가능
-//        self.navigationItem.title = "navigation title"
+        //        self.navigationItem.title = "navigation title"
         
-        self.title = "navigation title"
-        // optional value
-        self.navigationController?.navigationBar.barStyle = .default
-        // 상단 과 상태바의 간격 설정
-        self.navigationController?.navigationBar.isTranslucent = true
-        self.navigationController?.navigationBar.backgroundColor = .systemBrown
-        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        self.title = "네비게이션 타이틀"
+        
+        view.backgroundColor = .white
+        
+        let appearence = UINavigationBarAppearance()
+        appearence.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        appearence.backgroundColor = .systemBrown
+        self.navigationController?.navigationBar.scrollEdgeAppearance = appearence
         
         let leftButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(leftButtonTapped))
         self.navigationItem.leftBarButtonItem = leftButton
@@ -30,29 +31,27 @@ class ViewController: UIViewController {
         
         self.navigationItem.rightBarButtonItems = [rightButton, extraButton]
         
-        
         let button = UIButton(type: .system)
-        button.setTitle("next page", for: .normal)
-        button.addAction(UIAction { [ weak self] _ in
-            let nextViewController = UIViewController()
+        button.setTitle("다음 화면으로", for: .normal)
+        button.addAction(UIAction { [weak self] _ in
+            let nextViewController =  UIViewController()
+            nextViewController.view.backgroundColor = .white
             self?.show(nextViewController, sender: nil)
         }, for: .touchUpInside)
-        
         button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
         view.addSubview(button)
     }
-    // 연결되게 하려면 여기에 다른걸 link 하면 되겠지?
     
     @objc func leftButtonTapped() {
         print("왼쪽 버튼 탭됨")
     }
-
+    
     @objc func rightButtonTapped() {
         print("오른쪽 버튼 탭됨")
     }
-
+    
     @objc func extraButtonTapped() {
         print("추가 버튼 탭됨")
     }
-
+    
 }
