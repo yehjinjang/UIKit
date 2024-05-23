4b29d60143ce19d4d17dec1330c33a7ae242ef81
yehjinjang <j95033176@gmail.com>
2024-05-23 16:40:21 +0900

    navigation bar item added

--- Navigation/Navigation/ViewController.swift
+++ Navigation/Navigation/ViewController.swift
@@ -11,9 +11,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 둘 다 가능
+//        self.navigationItem.title = "navigation title"
+        
+        self.title = "navigation title"
+        // optional value
+        self.navigationController?.navigationBar.barStyle = .default
+        // 상단 과 상태바의 간격 설정
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.backgroundColor = .systemBrown
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        let leftButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(leftButtonTapped))
+        self.navigationItem.leftBarButtonItem = leftButton
+        
+        let rightButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(rightButtonTapped))
+        let extraButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(extraButtonTapped))
+        
+        self.navigationItem.rightBarButtonItems = [rightButton, extraButton]
+    }
+    
+    @objc func leftButtonTapped() {
+        print("왼쪽 버튼 탭됨")
     }
 
+    @objc func rightButtonTapped() {
+        print("오른쪽 버튼 탭됨")
+    }
+
+    @objc func extraButtonTapped() {
+        print("추가 버튼 탭됨")
+    }
 
 }
-
